42c89317fe6ca71bc9bb19a36eff569a46fd88ca
Andrii Zakharchenko <a.s.zakharchenko@globallogic.com>
2022-05-10 16:34:00 +0800

    refactor: russian bottlePlural func

--- Plural LocTests/RussianTest.swift
+++ Plural LocTests/RussianTest.swift
@@ -16,18 +16,17 @@ class RussianTest: BaseLanguageTest {
 
     func bottlePlural(bottles i: Int) -> String {
         var word: String!
-        if i <= 19, i >= 11 {
+        if i >= 11, i <= 19  {
             word = " бутылок"
         } else {
-            if i % 10 == 1 {
-                word = " бутылка"
-            } else {switch i % 10 {
-            case 2, 3, 4:
-                word = " бутылки"
-            default:
-                word = " бутылок"
+            switch i % 10 {
+                case 1:
+                    word = " бутылка"
+                case 2, 3, 4:
+                    word = " бутылки"
+                default:
+                    word = " бутылок"
                 }
-            }
         }
         return word
     }
